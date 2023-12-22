e68733027d9ff3a7f5fcf01cb6ed169fba1ea351
Jose Avalos <proyectos05@blueocean.com.py>
2023-12-22 07:21:48 -0300

    entrenamiento en mi compu con algoritmo de sort para tracking agregado

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO("yolov8n.pt")
-    model.train(data="train.yaml", epochs = 30 , imgsz=(640,640), batch = 4, optimizer="Adam")
+    model.train(data="train.yaml", epochs = 100 , imgsz=(640,640), batch = 32, optimizer="Adam")
